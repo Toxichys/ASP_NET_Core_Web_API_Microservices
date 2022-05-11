26c2599257fcb7542e78888c537133a48be87aec
Toxichys <ToxichSpam@bk.ru>
2022-05-11 16:29:30 +0200

    Урок 2

---
+++ Metrics/MetricsAgentTests/DotNetMetricsAgentTests.cs
@@ -0,0 +1,26 @@
+﻿using MetricsAgent.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Xunit;
+
+namespace MetricsAgentTests
+{
+    public class DotNetMetricsAgentTests
+    {
+        private DotNetMetricsController _dotNetMetricsController;
+        public DotNetMetricsAgentTests()
+        {
+            _dotNetMetricsController = new DotNetMetricsController();
+        }
+        [Fact]
+        public void GetMetrics_ReturnOk()
+        {
+            TimeSpan fromTime = TimeSpan.FromSeconds(0);
+            TimeSpan toTime = TimeSpan.FromSeconds(100);
+
+            IActionResult result = _dotNetMetricsController.GetMetrics(fromTime, toTime);
+
+            Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+}
---
+++ Metrics/MetricsManagerTests/HddMetricsManagerTests.cs
@@ -0,0 +1,28 @@
+﻿using MetricsManager.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Xunit;
+
+namespace MetricsManagerTests
+{
+    public class HddMetricsManagerTests
+    {
+        private HddMetricsController _hddMetricsController;
+        public HddMetricsManagerTests()
+        {
+            _hddMetricsController = new HddMetricsController();
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnOk()
+        {
+            int agentId = 1;
+            TimeSpan fromTime = TimeSpan.FromSeconds(0);
+            TimeSpan toTime = TimeSpan.FromSeconds(100);
+
+            IActionResult result = _hddMetricsController.GetMetricsFromAgent(agentId, fromTime, toTime);
+
+            Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+}
